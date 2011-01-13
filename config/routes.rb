1000bebc6efcffa1601a3c339eedecedf60b447a
Collin Schaafsma <collin@quickleft.com>
2011-01-12 21:13:21 -0700

    tweaking devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ KbombPhonegapServer::Application.routes.draw do
     resources :photos
   end
 
-  devise_for :users, :controllers => {:sessions => 'api/sessions'} do
+  devise_for :users
+
+  devise_for :users, :controllers => {:sessions => 'api/sessions'}, :skip => [:sessions] do
     match 'api/login' => 'api/sessions#create', :via => [:get, :post]
     get 'api/logout' => 'api/sessions#destroy', :as => :destroy_user_session
   end
